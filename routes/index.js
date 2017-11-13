7ecc2453137b67f3f35d23a7fd831e9e1cefd2b6
Tiffany <twang13@horizon.csueastbay.edu>
2017-11-12 19:41:51 -0800

    5th commit

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,13 @@ var querystring = require('querystring'); // for use in GET Query string of form
 router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
 
-// GET with URI /read/CS3520
-router.get('http://csweb01.csueastbay.edu/~rd7359/PHPandNode/getMyNodeResults.php', function(req, res, next) {
+// process post
+router.post('/readNameAndRespond', function(req, res, next) {
     //expecting data variable called name --retrieve value using body-parser
     var body = JSON.stringify(req.body);  //if wanted entire body as JSON
     var params = JSON.stringify(req.params);//if wanted parameters
-    var value_name = req.params.name;  //retrieve the data associated with name
-    res.send("hello " + value_name);
-})
-
-// process post
-router.post('http://whateverserver.com/readNameAndRespond', function(req, res, next) {
-    // retrieve value using body-parser
-    var value_name = req.body.name;  // retrieve data associated with name
-    res.send("hello " + value_name);
+    var value_name = req.body.name;  //retrieve the data associated with name
+    res.send("hello now " + value_name);
 });
 
 module.exports = router;
